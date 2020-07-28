f98e34f75958a19f5ffbb03de053105c136afa33
Elliot <marcoboutell@gmail.com>
2020-07-28 16:32:46 -0400

    finished moving smile

--- 00-MovingSmile/MovingSmile.py
+++ 00-MovingSmile/MovingSmile.py
@@ -7,13 +7,26 @@ def main():
     pygame.display.set_caption("Moving Smile")
     screen = pygame.display.set_mode((640, 480))
 
+    eye_y = 0
+    eye_x = 0
+    clock = pygame.time.Clock()
+
     while True:
-        # TODO 4: Set the clock speed to 60 fps
+        # Done 4: Set the clock speed to 60 fps
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 3: Make the eye pupils move with Up, Down, Left, and Right keys
-
+            # DOne 3: Make the eye pupils move with Up, Down, Left, and Right keys
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_UP]:
+                eye_y = eye_y - 5
+            if pressed_keys[pygame.K_DOWN]:
+                eye_y = eye_y + 5
+            if pressed_keys[pygame.K_LEFT]:
+                eye_x = eye_x - 5
+            if pressed_keys[pygame.K_RIGHT]:
+                eye_x = eye_x + 5
         screen.fill((255, 255, 255))  # white
 
         # API --> pygame.draw.circle(screen, color, (x, y), radius, thickness)
@@ -23,11 +36,11 @@ def main():
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (242 + eye_x, 162 + eye_y), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (398 + eye_x, 162 + eye_y), 7)  # black pupil
 
         # Done 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
