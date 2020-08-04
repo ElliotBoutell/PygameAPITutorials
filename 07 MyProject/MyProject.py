dd9fb3f7a1bf9e02156ba104a2f98779192f5426
Elliot <marcoboutell@gmail.com>
2020-08-04 16:12:52 -0400

    background pictures and text

--- 07 MyProject/MyProject.py
+++ 07 MyProject/MyProject.py
@@ -90,13 +90,40 @@ def display_welcome(clock, screen):
             pressed_keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.KEYDOWN and pressed_keys[pygame.K_s]:
+            if event.type == pygame.KEYDOWN and pressed_keys[pygame.K_SPACE]:
                 return
         screen.fill((0, 0, 0))
+        background = pygame.image.load("menu_screen.png")
+        screen.blit(background, (0, 0))
         font1 = pygame.font.Font(None, 50)
-        message_text = "Press S to Start"
-        message_image = font1.render(message_text, True, (255, 255, 0))
-        screen.blit(message_image, (190, 600))
+        message_text1 = "Press Space to Continue"
+        message_image1 = font1.render(message_text1, True, (255, 255, 0))
+        screen.blit(message_image1, (170, 600))
+        font2 = pygame.font.Font(None, 50)
+        message_text2 = "Welcome to Local Legends Disc Golf"
+        message_image2 = font2.render(message_text2, True, (255, 255, 0))
+        screen.blit(message_image2, (50, 100))
+        pygame.display.update()
+
+
+def choose_player(clock, screen):
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            pressed_keys = pygame.key.get_pressed()
+            if event.type == pygame.KEYDOWN and pressed_keys[pygame.K_SPACE]:
+                return
+        screen.fill((0, 0, 0))
+        background = pygame.image.load("name_screen.png")
+        screen.blit(background, (0, 0))
+        font1 = pygame.font.Font(None, 50)
+        message_text1 = "Type Name:"
+        message_image1 = font1.render(message_text1, True, (255, 255, 0))
+        screen.blit(message_image1, (25, 25))
+        font2 = pygame.font.Font(None, 50)
+        message_text2 = "Press Space to Continue"
+        message_image2 = font2.render(message_text2, True, (255, 255, 0))
+        screen.blit(message_image2, (170, 600))
         pygame.display.update()
 
 
@@ -110,10 +137,16 @@ def display_leaderboard(clock, screen):
             if event.type == pygame.KEYDOWN and pressed_keys[pygame.K_SPACE]:
                 return
         screen.fill((0, 0, 0))
+        background = pygame.image.load("end_screen.png")
+        screen.blit(background, (0, 0))
         font1 = pygame.font.Font(None, 30)
-        message_text = "Leaderboard"
-        message_image = font1.render(message_text, True, (255, 255, 0))
-        screen.blit(message_image, (25, 25))
+        message_text1 = "Leaderboard"
+        message_image1 = font1.render(message_text1, True, (255, 255, 0))
+        screen.blit(message_image1, (25, 25))
+        font2 = pygame.font.Font(None, 50)
+        message_text2 = "Press Space to Restart"
+        message_image2 = font2.render(message_text2, True, (255, 255, 0))
+        screen.blit(message_image2, (170, 600))
         pygame.display.update()
 
 
@@ -125,6 +158,7 @@ def main():
     pygame.display.update()
     while True:
         display_welcome(clock, screen)
+        choose_player(clock, screen)
         display_leaderboard(clock, screen)
 
 
